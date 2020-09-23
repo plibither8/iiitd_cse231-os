8e9ff0812ed6cf5c98b04e32a5c1a7c3ec9f3c1b
Mihir Chaturvedi <plibither8@gmail.com>
2020-09-23 21:53:38 +0530

    tests and p1 of A1

---
+++ assignments/1/p1/main.c
@@ -0,0 +1,171 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+// Error message constants
+char *ERR_FILE_OPEN = "ERR! File could not be opened properly.";
+char *ERR_FILE_SEEK = "ERR! Could not seek in file.";
+char *ERR_FILE_READ = "ERR! Coult not read file.";
+char *ERR_FILE_CLOSE = "ERR! Could not close file.";
+char *ERR_FORK = "ERR! Could not fork parent.";
+
+// Simple wrapper to take in error message
+// and print out to stderr stream
+void throw_error(char *msg)
+{
+  fprintf(stderr, "%s\n", msg);
+  exit(1);
+}
+
+// Open the csv file and produce error
+// if it couldn't be opened
+int open_file()
+{
+  int fd = open("marks.csv", O_RDONLY | O_EXCL);
+  if (fd == -1) throw_error(ERR_FILE_OPEN);
+
+  return fd;
+}
+
+// Get the file length (number of bytes)
+// to be used to print the content
+int get_file_length(int fd)
+{
+  int file_length = (int) lseek(fd, 0, SEEK_END);
+  if (file_length == -1) throw_error(ERR_FILE_SEEK);
+
+  // Go back to the start of the file
+  int seek_to_front = (int) lseek(fd, 0, SEEK_SET);
+  if (seek_to_front == -1) throw_error(ERR_FILE_SEEK);
+
+  return file_length;
+}
+
+// Read file from file descriptor and file length
+char * read_file(int fd, int file_length)
+{
+  // Allocate memory for file content
+  char *buf = (char *) calloc(file_length, sizeof(char));
+
+  // Read the file
+  int read_size = read(fd, buf, file_length);
+  if (read_size == -1) throw_error(ERR_FILE_READ);
+
+  return buf;
+}
+
+// Close file from file descriptor
+void close_file(int fd)
+{
+  int close_file = close(fd);
+  if (close_file == -1) throw_error(ERR_FILE_CLOSE);
+}
+
+void parse_line(char *line, char section)
+{
+  int student_id, a1, a2, a3, a4;
+  char student_section;
+  char *saveptr;
+
+  // Student ID
+  char *token = strtok_r(line, ",", &saveptr);
+  student_id = atoi(token);
+
+  // Student section
+  token = strtok_r(NULL, ",", &saveptr);
+  student_section = token[0];
+
+  // If the student section is not same
+  // as the requested section, return
+  if (student_section != section) return;
+
+  // Assignment 1
+  token = strtok_r(NULL, ",", &saveptr);
+  a1 = atoi(token);
+
+  // Assignment 2
+  token = strtok_r(NULL, ",", &saveptr);
+  a2 = atoi(token);
+
+  // Assignment 3
+  token = strtok_r(NULL, ",", &saveptr);
+  a3 = atoi(token);
+
+  // Assignment 4
+  token = strtok_r(NULL, ",", &saveptr);
+  a4 = atoi(token);
+
+  // Calculate average
+  float average  = (float)(a1 + a2 + a3 + a4) / 4;
+
+  printf("%d: %f\n", student_id, average);
+}
+
+void parse_file(char *file_content, char section)
+{
+  int count = -1;
+  char *saveptr;
+  char *line = strtok_r(file_content, "\n", &saveptr);
+
+  while(line)
+  {
+    // Increment the counter
+    count++;
+
+    // Skip the initial CSV header
+    if (count == 0)
+    {
+      line = strtok_r(NULL, "\n", &saveptr);
+      continue;
+    }
+
+    // Parse the current line
+    parse_line(line, section);
+
+    // Next line
+    line = strtok_r(NULL, "\n", &saveptr);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  // Fork the process and store child pid
+  int child_process = fork();
+
+  // Error in forking
+  if (child_process < 0)
+  {
+    throw_error(ERR_FORK);
+  }
+  else if (child_process == 0)
+  { // Child process
+    int fd = open_file();
+    int file_length = get_file_length(fd);
+    char *file_content = read_file(fd, file_length);
+
+    printf("Section A:\n");
+    parse_file(file_content, 'A');
+
+    close_file(fd);
+    exit(0);
+  }
+  else
+  { // Parent process
+    waitpid(child_process, NULL, 0);
+
+    int fd = open_file();
+    int file_length = get_file_length(fd);
+    char *file_content = read_file(fd, file_length);
+
+    printf("\n\nSection B:\n");
+    parse_file(file_content, 'B');
+
+    close_file(fd);
+  }
+
+  return 0;
+}
---
+++ tests/wait.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Hello, PID: %d\n", (int)getpid());
+  int parent_pid = (int)getpid();
+  int child_process = fork();
+  printf("Poop PID: %d\n", getpid());
+  if(child_process < 0) 
+  {
+    fprintf(stderr, "There seems to be an error in creating a child process\n");
+    exit(1);
+  }
+  else if (child_process == 0)
+  {
+    printf("Yayy, I'm the child :D, PID: %d\n", (int)getpid());
+    exit(0);
+  }
+  else
+  {
+    int parent_wait = wait(NULL);
+    printf("Hmm, still the parent here, PID: %d\n", (int)getpid());
+  }
+  return 0;
+}
