b6801fb6545cda6605c0cb6f5b4381400ca6dc68
Mihir Chaturvedi <plibither8@gmail.com>
2020-10-28 10:18:30 +0000

    Fix: Add A2:Q2 stuff

--- assignments/2/Q2/test.c
+++ assignments/2/Q2/test.c
@@ -1,2 +1,19 @@
-/* Name: Mihir Chaturvedi
-   Roll_Number: 2019061 */
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wait.h>
+
+#define SYS_SH_TASK_INFO 440
+
+int main(int argc, char **argv)
+{
+  char *filename = "task-info.txt";
+  int pid = 1;
+
+  syscall(SYS_SH_TASK_INFO, pid, filename);
+
+  return 0;
+}
