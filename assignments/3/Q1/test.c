d7977d7dba1e360b64298c88d352f1342da799fa
Mihir Chaturvedi <plibither8@gmail.com>
2020-11-12 05:44:01 +0000

    fix: add some stuff to the test file

--- assignments/3/Q1/test.c
+++ assignments/3/Q1/test.c
@@ -12,17 +12,17 @@
 void long_computation()
 {
   long long sum = 0;
-  for (int i = 0; i < 1000000000; i++)
+  for (long i = 0; i < 1000000000; i++)
     sum += i;
 }
 
 int main(int argc, char **argv)
 {
-  struct timeval start_time, stop_time;
-  gettimeofday(&start_time, NULL);
-
+  float duration;
   int rtnice_val = 100;
   int child_pid = fork();
+  double cpu_time_used;
+  clock_t end, start = clock();
 
   switch (child_pid)
   {
@@ -30,21 +30,24 @@ int main(int argc, char **argv)
       break;
     }
     case 0: {
-      printf("Child process: PID %d\n", child_pid);
-      syscall(RTNICE_ID, child_pid, rtnice_val);
+      int pid = getpid();
+      printf("Child process: PID %d\n", pid);
+      syscall(RTNICE_ID, pid, rtnice_val);
       long_computation();
+      end = clock();
+      cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+      printf("Task duration for child: %f seconds\n", cpu_time_used);
       break;
     }
     default: {
       printf("Parent process: PID %d\n", (int)getpid());
       long_computation();
+      end = clock();
+      cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+      printf("Task duration for parent: %f seconds\n", cpu_time_used);
       wait(NULL);
     }
   }
-
-  gettimeofday(&stop_time, NULL);
-  unsigned long duration = stop_time.tv_usec - start_time.tv_usec;
-  printf("Task duration for PID %d: %lu\n", (int)getpid(), duration);
 
   return 0;
 }
