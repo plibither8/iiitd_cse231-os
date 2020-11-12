d1e9f2e89430f86e4a9ad1de095664eab9f73b14
Mihir Chaturvedi <plibither8@gmail.com>
2020-11-12 12:52:26 +0000

    Fix: makefile and test.c

--- assignments/3/Q1/test.c
+++ assignments/3/Q1/test.c
@@ -1,11 +1,14 @@
 /* Name: Mihir Chaturvedi
    Roll_Number: 2019061 */
-#include <stdio.h>
-#include <sys/syscall.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/time.h>
 #include <wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/kernel.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <time.h>
+#include <omp.h>
 
 #define RTNICE_ID 440
 
@@ -13,40 +16,68 @@ void long_computation()
 {
   long long sum = 0;
   for (long i = 0; i < 1000000000; i++)
-    sum += i;
+    sum++;
 }
 
 int main(int argc, char **argv)
 {
-  float duration;
-  int rtnice_val = 100;
-  int child_pid = fork();
-  double cpu_time_used;
-  clock_t end, start = clock();
+  int rtnice_step, fork_count, i;
 
-  switch (child_pid)
+  double duration;
+  double end, start;
+
+  printf("rtnice value step-size: ");
+  scanf("%d", &rtnice_step);
+  printf("Number of forks/children: ");
+  scanf("%d", &fork_count);
+
+  printf("With rtnice=0:\n");
+  for (i = 0; i < fork_count; i++)
   {
-    case -1: {
-      break;
-    }
-    case 0: {
+    if (!fork()) {
       int pid = getpid();
-      printf("Child process: PID %d\n", pid);
-      syscall(RTNICE_ID, pid, rtnice_val);
+
+      if (syscall(RTNICE_ID, pid, 0)) return -1;
+
+      start = omp_get_wtime();
       long_computation();
-      end = clock();
-      cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-      printf("Task duration for child: %f seconds\n", cpu_time_used);
-      break;
+      end = omp_get_wtime();
+      duration = end - start;
+
+      printf("Child process %d; rtnice=%d; duration=%lf seconds\n", i + 1, 0, duration);
+      exit(0);
     }
-    default: {
-      printf("Parent process: PID %d\n", (int)getpid());
+  }
+
+  while(i > 0)
+  {
+    wait(NULL);
+    i--;
+  }
+
+  printf("With increasing rtnice:\n");
+  for (i = 0; i < fork_count; i++)
+  {
+    if (!fork()) {
+      int pid = getpid();
+      int rtnice_val = i * rtnice_step;
+
+      if (syscall(RTNICE_ID, pid, rtnice_val)) return -1;
+
+      start = omp_get_wtime();
       long_computation();
-      end = clock();
-      cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-      printf("Task duration for parent: %f seconds\n", cpu_time_used);
-      wait(NULL);
+      end = omp_get_wtime();
+      duration = end - start;
+
+      printf("Child process %d; rtnice=%d; duration=%lf seconds\n", i + 1, rtnice_val, duration);
+      exit(0);
     }
+  }
+
+  while(i > 0)
+  {
+    wait(NULL);
+    i--;
   }
 
   return 0;
